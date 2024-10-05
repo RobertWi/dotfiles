7936b50ae14e481b74a93a7d968a9ee9c0a9fef5
RobertWi <1311049+RobertWi@users.noreply.github.com>
2024-10-05 14:15:05 +0200

    init

---
+++ host/default.nix
@@ -0,0 +1,35 @@
+# The Looking Glass
+# System configuration for my work Macbook
+
+{ inputs, globals, overlays, ... }:
+
+with inputs;
+
+darwin.lib.darwinSystem {
+  system = "x86_64-darwin";
+  specialArgs = { };
+  modules = [
+    ../../modules/common
+    ../../modules/darwin
+    (globals // rec {
+      user = "RobertWi";
+      gitName = "RobertWi;
+      gitEmail = "1311049+RobertWi@users.noreply.github.com";
+    })
+    home-manager.darwinModules.home-manager
+    {
+      nixpkgs.overlays = [ firefox-darwin.overlay ] ++ overlays;
+      networking.hostName = "MacProM3";
+      identityFile = "/Users/Robert.Winder/.ssh/id_ed25519";
+      gui.enable = true;
+      mail.user = globals.user;
+      firefox.enable = true;
+      dotfiles.enable = true;
+      nixlang.enable = true;
+      terraform.enable = true;
+      python.enable = true;
+      lua.enable = true;
+      kubernetes.enable = true;
+    }
+  ];
+}
