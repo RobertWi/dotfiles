8410cb47409f5d98f312037a9c3cd074c8f569e1
RobertWi <1311049+RobertWi@users.noreply.github.com>
2024-10-05 15:30:46 +0200

    future to be used all systems

--- flake.nix
+++ flake.nix
@@ -71,6 +71,14 @@
         (import ./overlays/calibre-web.nix)
       ];
 
+
+      # System types to support.
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
     in rec {
 
       # Contains my full Mac system builds, including home-manager
