2eb3ffc079526dcdb03432a6909ab07436484c84
RobertWi <1311049+RobertWi@users.noreply.github.com>
2024-10-06 13:04:59 +0200

    set another global

--- hosts/MacProM3/default.nix
+++ hosts/MacProM3/default.nix
@@ -3,12 +3,28 @@
 
 { inputs, globals, overlays, ... }:
 
+  globals
+      // rec {
+        user = "Noah.Masur";
+        gitName = "Noah-Masur_1701";
+        gitEmail = "${user}@take2games.com";
+      }
+    )
+
 inputs.darwin.lib.darwinSystem {
   system = "aarch64-darwin";
   specialArgs = { };
   modules = [
     ../../modules/common
     ../../modules/darwin
+    ( #something is wrong here not using global set in flake.nix 
+    globals
+      // rec {
+        user = "robertwinder";
+        gitName = "RobertWi";
+        gitEmail = "${gitName}@take2games.com";
+      }
+    )  
 
     inputs.home-manager.darwinModules.home-manager
     inputs.mac-app-util.darwinModules.default
