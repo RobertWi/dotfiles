ef677804a7c0b21f5d4770c123dbaacc11b90326
RobertWi <1311049+RobertWi@users.noreply.github.com>
2024-10-06 08:55:20 +0200

    xtra modules

---
+++ modules/nixos/system/default.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }: {
+
+  imports = [ ./doas.nix ./journald.nix ./user.nix ];
+
+  config = lib.mkIf pkgs.stdenv.isLinux {
+
+    # Pin a state version to prevent warnings
+    system.stateVersion =
+      config.home-manager.users.${config.user}.home.stateVersion;
+
+    # This setting only applies to NixOS, different on Darwin
+    nix.gc.dates = "weekly";
+
+    systemd.timers.nix-gc.timerConfig = { WakeSystem = true; };
+    systemd.services.nix-gc.postStop =
+      lib.mkIf (!config.server) "systemctl suspend";
+
+  };
+
+}
---
+++ modules/nixos/system/doas.nix
@@ -0,0 +1,35 @@
+# Replace sudo with doas
+
+{ config, pkgs, lib, ... }: {
+
+  config = lib.mkIf pkgs.stdenv.isLinux {
+
+    security = {
+
+      # Remove sudo
+      sudo.enable = false;
+
+      # Add doas
+      doas = {
+        enable = true;
+
+        # No password required
+        wheelNeedsPassword = false;
+
+        # Pass environment variables from user to root
+        # Also requires removing password here
+        extraRules = [{
+          groups = [ "wheel" ];
+          noPass = true;
+          keepEnv = true;
+        }];
+      };
+    };
+
+    home-manager.users.${config.user}.programs.zsh.shellAliases = {
+      sudo = "doas";
+    };
+
+  };
+
+}
