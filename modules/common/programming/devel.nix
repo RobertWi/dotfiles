fef72c68e0f6cc08e0c9a7130121c65870ad3d6f
RobertWi <1311049+RobertWi@users.noreply.github.com>
2024-10-07 15:49:03 +0200

    both?

--- modules/common/programming/devel.nix
+++ modules/common/programming/devel.nix
@@ -2,9 +2,12 @@
 
   unfreePackages = [ 
     "vscode-with-extensions"
+    "vscode" 
   ];
+  
     home-manager.users.${config.user} = {
       home.packages = with pkgs; [
+        vscode
         vscode-with-extensions
         vscode-extensions.hashicorp.hcl
         vscode-extensions.github.copilot #private use not allowed on work repos
