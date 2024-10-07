251f4f5dadc85fdbd80c9391175dc668e8063e22
RobertWi <1311049+RobertWi@users.noreply.github.com>
2024-10-07 16:15:07 +0200

    don't bark

--- modules/common/programming/devel.nix
+++ modules/common/programming/devel.nix
@@ -2,12 +2,10 @@
 
   unfreePackages = [ 
     "vscode-with-extensions"
-    "vscode"
   ];
   
     home-manager.users.${config.user} = {
       home.packages = with pkgs; [
-        vscode
         vscode-with-extensions
         vscode-extensions.hashicorp.hcl   
         vscode-extensions.hashicorp.terraform
