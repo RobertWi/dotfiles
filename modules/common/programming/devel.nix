1cdf6d7059eef8e0227fb5834c7edcb2eb9cd79d
RobertWi <1311049+RobertWi@users.noreply.github.com>
2024-10-07 16:04:52 +0200

    no github co pilot

--- modules/common/programming/devel.nix
+++ modules/common/programming/devel.nix
@@ -3,19 +3,15 @@
   unfreePackages = [ 
     "vscode-with-extensions"
     "vscode"
-    "vscode-extensions.github.copilot"
-    "vscode-extensions.github.chat"
   ];
   
     home-manager.users.${config.user} = {
       home.packages = with pkgs; [
         vscode
         vscode-with-extensions
-        vscode-extensions.hashicorp.hcl
-        vscode-extensions.github.copilot #private use not allowed on work repos
-        vscode-extensions.github.copilot-chat #private use not allowed on work repos    
+        vscode-extensions.hashicorp.hcl   
         vscode-extensions.hashicorp.terraform
-
+        #more to come
       ];
 
     };
