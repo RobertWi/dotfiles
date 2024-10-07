d49d5f96f3201eb3136f0836b3e1b09710c0f10b
RobertWi <1311049+RobertWi@users.noreply.github.com>
2024-10-07 15:54:42 +0200

    did read wrong set unfree to github plugins

--- modules/common/programming/devel.nix
+++ modules/common/programming/devel.nix
@@ -2,12 +2,12 @@
 
   unfreePackages = [ 
     "vscode-with-extensions"
-    "vscode" 
+    "vscode-extensions.github.copilot"
+    "vscode-extensions.github.chat"
   ];
   
     home-manager.users.${config.user} = {
       home.packages = with pkgs; [
-        vscode
         vscode-with-extensions
         vscode-extensions.hashicorp.hcl
         vscode-extensions.github.copilot #private use not allowed on work repos
