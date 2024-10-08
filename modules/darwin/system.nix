26c2caf864825d5a8eef6d7dc2df055ed27f8250
RobertWi <1311049+RobertWi@users.noreply.github.com>
2024-10-08 07:34:41 +0200

    redo

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -174,25 +174,25 @@
         };
       };
 
-      # create symlinks to let apps appear in spotlight
-      activationScripts.applications.text = let
+      system.activationScripts.applications.text = let
         env = pkgs.buildEnv {
           name = "system-applications";
           paths = config.environment.systemPackages;
           pathsToLink = "/Applications";
         };
-      in 
-        pkgs.lib.mkForce '' 
-        echo "setting up /Applications...." >&2
+      in
+        pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
         rm -rf /Applications/Nix\ Apps
         mkdir -p /Applications/Nix\ Apps
-        find ${env}/Applications -maxdepth 1 -type l -exec readling '{} + |
-        while read src; do 
-          app_name$(basename "$src")
-          echo "copying "$src" >&2 
-          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name" 
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
         done
-      '' ;  
+      '';
   
       # Settings that don't have an option in nix-darwin
       activationScripts.postActivation.text = ''
