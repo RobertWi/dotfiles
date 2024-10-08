d4a4f3beae49edaca222d571283b4da4229c08b6
Robert Winder <user.email " robert.winder@rabobank.nl>
2024-10-08 23:21:10 +0200

    nerdctl

--- modules/common/programming/virtualisation.nix
+++ modules/common/programming/virtualisation.nix
@@ -17,7 +17,7 @@
         # limactl start --vm-type=vz template://ubuntu-lts 
         # a vm of choice in seconds https://lima-vm.io/docs/templates/ 
         lima
-        nerctl
+        nerdctl
         qemu
         podman
       ];  
